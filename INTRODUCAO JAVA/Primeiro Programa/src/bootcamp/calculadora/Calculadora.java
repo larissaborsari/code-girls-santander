2fd6266fc1f0366e17d17c2f7ef2be63986eecf8
larissa.borsari <larissaborsari@hotmail.com>
2022-07-13 16:28:07 -0300

    last commit

--- INTRODUCAO JAVA/Primeiro Programa/src/bootcamp/calculadora/Calculadora.java
+++ INTRODUCAO JAVA/Primeiro Programa/src/bootcamp/calculadora/Calculadora.java
@@ -7,37 +7,37 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		int a, b;
+		double a, b;
 		
 		System.out.println("Digite o primeiro valor: ");
 		a = input.nextInt();
 		System.out.println("Digite o segundo valor: ");
 		b = input.nextInt();
 		
-		int soma = soma(a,b);
-		int subtracao = subtracao(a,b);
-		int multiplicacao = multiplicacao(a,b);
-		int divisao = divisao(a,b);
+		double soma = soma(a,b);
+		double subtracao = subtracao(a,b);
+		double multiplicacao = multiplicacao(a,b);
+		double divisao = divisao(a,b);
 		
-		System.out.println(soma);
-		System.out.println(subtracao);
-		System.out.println(multiplicacao);
-		System.out.println(divisao);
+		System.out.println("soma " + soma);
+		System.out.println("subtracao " + subtracao);
+		System.out.println("multiplicacao " + multiplicacao);
+		System.out.println("divisao " + divisao);
 	}
 
-	public static int soma(int a, int b) {
+	public static double soma(double a, double b) {
 		return a + b;
 	}
 	
-	public static int subtracao(int a, int b) {
+	public static double subtracao(double a, double b) {
 		return a - b;
 	}
 	
-	public static int multiplicacao(int a, int b) {
+	public static double multiplicacao(double a, double b) {
 		return a * b;
 	}
 	
-	public static int divisao(int a, int b) {
+	public static double divisao(double a, double b) {
 		return a / b;
 	}
 	
