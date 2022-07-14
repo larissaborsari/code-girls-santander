b01c48a138f44c2ce67ae137f2a05ebf4e59451b
larissa.borsari <larissaborsari@hotmail.com>
2022-07-14 14:22:15 -0300

    last commit

---
+++ INTRODUCAO JAVA/VARIAVEIS - TIPOS DE DADOS - OPERADORES/Variaveis/src/bootcamp/modII/Main.java
@@ -0,0 +1,28 @@
+package bootcamp.modII;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i;
+        int I;
+
+        i = 5;
+        I = 10;
+
+        final int j = 10;
+
+        int abcd1234 = 8;
+
+        int quantidadeProduto = 50;
+
+        final int NUMERO_TENTATIVAS = 2;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(j);
+        System.out.println(abcd1234);
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+
+    }
+}
