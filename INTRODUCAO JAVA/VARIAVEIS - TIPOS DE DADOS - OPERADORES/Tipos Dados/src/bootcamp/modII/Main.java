d23e0d7876569e41ae00dc005b54f0093fdd3ecf
larissa.borsari <larissaborsari@hotmail.com>
2022-07-14 14:50:55 -0300

    last commit

---
+++ INTRODUCAO JAVA/VARIAVEIS - TIPOS DE DADOS - OPERADORES/Tipos Dados/src/bootcamp/modII/Main.java
@@ -0,0 +1,53 @@
+package bootcamp.modII;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+
+        int i1 = -350;
+        int i2 = 58900;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 20045856320021585L;
+
+        float f1 = 4.5f;
+        float f2 = 10.68f;
+
+        double d1 = 85.69;
+        double d2 = 99.86d;
+
+        char c1 = 'M';
+        char c2 = '\u8857';
+
+        String st1 = "fulano";
+        String st2 = "teste teste teste";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+
+
+    }
+}
