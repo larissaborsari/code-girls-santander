7693ac930d7f224c37ee654c18a498e413cba9f1
larissa.borsari <larissaborsari@hotmail.com>
2022-07-15 15:22:38 -0300

    last commit

---
+++ INTRODUCAO JAVA/VARIAVEIS - TIPOS DE DADOS - OPERADORES/Operadores/operadores/src/bootcamp/Main.java
@@ -0,0 +1,31 @@
+package bootcamp;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int k = 10;
+
+        int i = ++ k;
+        int j = k --;
+        int x = k;
+
+        System.out.println("i " + i);
+        System.out.println("j " + j);
+        System.out.println("x " + x);
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int e = 50;
+
+        int r1 = a+b;
+        int r2 = c-a;
+        int r3 = d*b;
+        int r4 = e/a;
+        int r5 = c%b;
+
+    }
+
+}
