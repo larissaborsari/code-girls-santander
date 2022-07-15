36ab29df08439cdc7c19d54628ccf3198b6e35e1
larissa.borsari <larissaborsari@hotmail.com>
2022-07-15 17:08:56 -0300

    last commit

---
+++ INTRODUCAO JAVA/METODOS/exercicio1/src/bootcamp/Main.java
@@ -0,0 +1,22 @@
+package bootcamp;
+
+import javax.print.attribute.standard.MediaName;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Chamando o método
+        Calculadora.soma(3,6);
+        Calculadora.subtracao(5,2);
+        Calculadora.multiplicacao(8,5);
+        Calculadora.divisao(27,9);
+
+        //Mensagem
+
+        Mensagem.obterMensagem(13);
+        Mensagem.obterMensagem(8);
+        Mensagem.obterMensagem(21);
+
+    }
+}
