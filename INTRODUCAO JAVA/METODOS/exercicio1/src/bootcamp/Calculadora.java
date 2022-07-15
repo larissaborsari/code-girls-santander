36115d0f1843cb071b2ee260f272aea43e3c130b
larissa.borsari <larissaborsari@hotmail.com>
2022-07-15 16:51:39 -0300

    last commit

---
+++ INTRODUCAO JAVA/METODOS/exercicio1/src/bootcamp/Calculadora.java
@@ -0,0 +1,23 @@
+package bootcamp;
+
+public class Calculadora {
+    public static void soma(double a,double b) {
+        double resultado = a + b;
+        System.out.println("A soma de " + a + "mais " + b + "é igual a: " + resultado);
+    }
+
+    public static void subtracao(double a,double b) {
+        double resultado = a - b;
+        System.out.println("A subtração de " + a + "menos " + b + "é igual a: " + resultado);
+    }
+
+    public static void multiplicacao(double a,double b) {
+        double resultado = a * b;
+        System.out.println("A multiplicação de " + a + "vezes " + b + "é igual a: " + resultado);
+    }
+
+    public static void divisao(double a,double b) {
+        double resultado = a / b;
+        System.out.println("A divisão de " + a + "por " + b + "é igual a: " + resultado);
+    }
+}
